c7feb1b1cefce93afe13b9d6823c00d89b90baee
HIMANSHUMISHRA389 <himanshumishra389@gmail.com>
2023-03-12 13:10:43 +0530

    commited

--- UserManagementSystem/server.js
+++ UserManagementSystem/server.js
@@ -1,15 +1,35 @@
 const express=require("express");
 const dotenv=require("dotenv");
+const morgan=require("morgan");
+const bodyparser=require("body-parser");
+const path=require("path");
 
 
 dotenv.config({path:"config.env"});
 
 const app=express();
 const PORT=process.env.PORT||8080
-
+//log request
+app.use(morgan('tiny'));
 app.get('/',(req,res)=>{
-    res.send("CRUD Application");
+    res.render('index.ejs');
 })
+
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true}));
+
+
+//set view engine
+app.set('view engine','ejs');
+
+
+//load assets
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")));
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
+
 
 app.listen(PORT,()=>{
     console.log(`server is started at http://localhost:${PORT}`)
